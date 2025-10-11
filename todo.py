bb0b11cf71d66435434da7f0489d8ba96ed3f380
iliyayo <iliya.yousefi.real@gmail.com>
2025-10-11 18:59:33 -0400

    basic structure optimized

--- todo.py
+++ todo.py
@@ -1,12 +1,21 @@
 #defining classes project and task
 MAX_NUMBER_OF_PROJECT = 50
-class date:
+class Status:
 
+class Date:
+    year : int
+    month : int
+    day : int
 class Task():
-    title < 30
-    deadline date
-    deadline time
-    description < 150
+    title : str
+    deadline : Date
+    description : str
     Status : todo | doing | done
 class Project:
-    Task
+    Task list
+    deadline date
+    method >> create task
+    method >> delete task
+    method >> delete project
+    method >> create project
+
