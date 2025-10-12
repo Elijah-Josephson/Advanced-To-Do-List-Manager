e9da4f0dc86e17df42fbabb69d288c16ff3eb2d0
iliyayo <iliya.yousefi.real@gmail.com>
2025-10-12 09:00:48 -0400

    added argparse library

--- todo.py
+++ todo.py
@@ -1,4 +1,8 @@
 #defining classes project and task
+from argparse import ArgumentParser, Namespace
+
+from babel.dates import format_interval
+
 MAX_NUMBER_OF_PROJECT = 50
 class Status:
 
@@ -18,4 +22,12 @@ class Project:
     method >> delete task
     method >> delete project
     method >> create project
+
+parser = ArgumentParser()
+
+parser.add_argument('add_proj', help='Add a project',
+                    type= str)
+# adding verbose
+#parser.add_argument('-v', '--verbose', action=verb())
+args : Namespace = parser.parse_args()
 
