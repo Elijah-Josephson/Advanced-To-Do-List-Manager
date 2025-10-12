bf63838a85e909ffd1f04d2867be942d2e68f036
iliyayo <iliya.yousefi.real@gmail.com>
2025-10-12 11:12:41 -0400

    added __init__ functions and title/description setter methods for class Task and Project

--- todo.py
+++ todo.py
@@ -4,20 +4,28 @@ from argparse import ArgumentParser, Namespace
 from babel.dates import format_interval
 
 MAX_NUMBER_OF_PROJECT = 50
-class Status:
-
 class Date:
     year : int
     month : int
     day : int
-class Task():
-    title : str
+class Task:
+    def __init__(self, name : str):
+        self.title = name
     deadline : Date
     description : str
-    Status : todo | doing | done
-class Project(name : str):
-    Task list
-    deadline date
+    def set_description(self, desc : str):
+        self.description = desc
+    def set_status(self, num : int):
+        self.stat = num
+class Project:
+    def __init__(self, name : str):
+        Project.name = name
+    def set_description(self, desc : str) -> None:
+        Project.description = desc
+    tasks = []
+    Deadline : Date
+    def Add_Task(self, taskname : str):
+        new_task = Task()
     method >> create task
     method >> delete task
     method >> delete project
@@ -27,11 +35,11 @@ parser = ArgumentParser()
 
 parser.add_argument('add_proj', help='Add a project',
                     type= str)
-adding verbose
+#adding verbose
 parser.add_argument('-v', '--verbose', help = 'verbose description')
 args : Namespace = parser.parse_args()
 
 if args.add_proj:
-    proj = new Project(name = args.add_proj)
+    proj = Project()
 if args.verbose:
-    print(f'Project {args.add_proj} created succesfully!')
+    print(f'Project {args.add_proj} created successfully!')
