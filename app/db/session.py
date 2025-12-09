0e51af2e114c643978125f6b1483e90c1952c446
iliyayo <iliya.yousefi.real@gmail.com>
2025-12-09 16:25:35 +0330

    refactor

--- app/db/session.py
+++ app/db/session.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgresql://postgres:secret@localhost/alembic_db')
+engine = create_engine('postgresql+psycopg://todo_user:todo_pass@localhost:5432/todolist_db')
 Session = sessionmaker(bind = engine)
